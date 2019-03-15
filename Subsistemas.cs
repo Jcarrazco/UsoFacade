0de74765b3dcf34557ba05ece10e66621e07261a
Jcarrazco <48524432+Jcarrazco@users.noreply.github.com>
2019-03-15 16:43:08 -0600

    Add files via upload

---
+++ Subsistemas.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*------------------------------------Jorge Carrazco---------------------------------
+ * ----------------------------------Uso de los Desig Patterns-----------------------
+ * -----------------------------Facade Pattern---------------------------------------
+ * 
+ */
+namespace UsoFacade
+{
+    class CuentaBancaria
+    {
+
+        public double RestaCuenta(double a, double b )
+        {
+            Console.WriteLine("El total nuevo es: " + Convert.ToString(a - b));
+            return a - b;  
+        }
+
+        public double SumaCuenta(double a, double b)
+        {
+            Console.WriteLine("El total nuevo es: " + Convert.ToString(a + b));
+            return a + b;
+        }
+    }
+
+    class Usuario
+    {
+        
+        public void Quien(string Nombre, int Ncuenta)
+        {
+            Console.WriteLine("El usuario: "+ Nombre+ " Ah realizado una transaccion " +
+                " de la cuenta "+Ncuenta);
+        }
+    }    
+}
---
+++ facade.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoFacade
+{
+    public class Facade
+    {
+        CuentaBancaria _cuenta = new CuentaBancaria();
+        Usuario _usuario = new Usuario();
+        
+
+        public void operacion1(string Name, int cuenta, double total, double v1)
+        {
+            _cuenta.SumaCuenta(total,v1);
+            _usuario.Quien(Name, cuenta);
+        }
+
+        public void operacion2(string Name, int cuenta, double total, double v1)
+        {
+            _cuenta.RestaCuenta(total, v1);
+            _usuario.Quien(Name, cuenta);
+        }
+    }
+
+}
