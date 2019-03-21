031fc888073ef3027282bbfcc6325855b11a4d77
Jcarrazco <48524432+Jcarrazco@users.noreply.github.com>
2019-03-21 17:03:10 -0600

    Add files via upload

---
+++ UsoFacade/UsoFacade/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsoFacade
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string Name = "Jose";
+            int cuenta = 325665;
+            double Tot = 5000;
+            string answer;
+            Facade f = new Facade();
+            Console.WriteLine("Menu de Operaciones \n" +
+                "1. Depositar\n" +
+                "2. Retirar");
+            answer = Console.ReadLine();
+            if (answer == "1")
+            {
+                Console.WriteLine("Tecle la cantidad");
+                double c = Convert.ToDouble(Console.ReadLine());
+                f.operacion1(Name,cuenta,Tot,c);
+            }
+            else if (answer == "2")
+            {
+                Console.WriteLine("Tecle la cantidad");
+                double c = Convert.ToDouble(Console.ReadLine());
+                f.operacion1(Name, cuenta, Tot, c);
+            }
+            Console.ReadKey();
+        }
+    }
+}
